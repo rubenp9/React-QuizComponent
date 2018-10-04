fb0ad3181b960994f854572cb242b1e04e0395f3
ruben.perez <ruben28693@gmail.com>
2018-10-04 13:30:33 +0200

    .

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -9,7 +9,7 @@ class QuizEnd extends Component {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a href='#' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+        <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
       </div>
     )
   }
